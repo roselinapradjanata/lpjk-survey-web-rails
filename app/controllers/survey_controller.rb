c9cae2faffa29c55ee94561cfe800fdaf94c2b6c
roselinapradjanata <roselinapradjanata@gmail.com>
2018-07-25 17:50:54 +0700

    Create route for get all form data

---
+++ app/controllers/survey_controller.rb
@@ -0,0 +1,48 @@
+class SurveyController < ApplicationController
+
+	def index
+		company_categories = CompanyCategoryService.get_all
+		company_types = CompanyTypeService.get_all
+		construction_product_types = ConstructionProductTypeService.get_all
+		technology_construction_assessment_methods = TechnologyConstructionAssessmentMethodService.get_all
+		technology_construction_assessments = TechnologyConstructionAssessmentService.get_all
+		technology_construction_benefits = TechnologyConstructionBenefitService.get_all
+		technology_construction_categories = TechnologyConstructionCategoryService.get_all
+		technology_construction_costs = TechnologyConstructionCostService.get_all
+		technology_construction_human_resources = TechnologyConstructionHumanResourceService.get_all
+		technology_construction_inovation_categories = TechnologyConstructionInovationCategoryService.get_all
+		technology_construction_inovation_origins = TechnologyConstructionInovationOriginService.get_all
+		technology_construction_obstacles = TechnologyConstructionObstacleService.get_all
+		technology_construction_origins = TechnologyConstructionOriginService.get_all
+		technology_construction_owners = TechnologyConstructionOwnerService.get_all
+		technology_construction_primary_types = TechnologyConstructionPrimaryTypeService.get_all
+		technology_construction_stages = TechnologyConstructionStageService.get_all
+		technology_construction_success_factors = TechnologyConstructionSuccessFactorService.get_all
+		technology_construction_supply_chain_origins = TechnologyConstructionSupplyChainOriginService.get_all
+		technology_construction_supply_chains = TechnologyConstructionSupplyChainService.get_all
+		technology_construction_types = TechnologyConstructionTypeService.get_all
+		return render json: {
+			company_categories: company_categories,
+			company_types: company_types,
+			construction_product_types: construction_product_types,
+			technology_construction_assessment_methods: technology_construction_assessment_methods,
+			technology_construction_assessments: technology_construction_assessments,
+			technology_construction_benefits: technology_construction_benefits,
+			technology_construction_categories: technology_construction_categories,
+			technology_construction_costs: technology_construction_costs,
+			technology_construction_human_resources: technology_construction_human_resources,
+			technology_construction_inovation_categories: technology_construction_inovation_categories,
+			technology_construction_inovation_origins: technology_construction_inovation_origins,
+			technology_construction_obstacles: technology_construction_obstacles,
+			technology_construction_origins: technology_construction_origins,
+			technology_construction_owners: technology_construction_owners,
+			technology_construction_primary_types: technology_construction_primary_types,
+			technology_construction_stages: technology_construction_stages,
+			technology_construction_success_factors: technology_construction_success_factors,
+			technology_construction_supply_chain_origins: technology_construction_supply_chain_origins,
+			technology_construction_supply_chains: technology_construction_supply_chains,
+			technology_construction_types: technology_construction_types
+		}, status: :ok
+	end
+
+end
