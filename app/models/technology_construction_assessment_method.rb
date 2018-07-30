b52ec4d40cf0597743627fa71c2b043e86c641f8
roselinapradjanata <roselinapradjanata@gmail.com>
2018-07-30 12:23:19 +0700

    Delete technology construction assessment method

--- app/models/technology_construction_assessment_method.rb
+++
@@ -1,2 +0,0 @@
-class TechnologyConstructionAssessmentMethod < ApplicationRecord
-end
--- app/services/technology_construction_assessment_method_service.rb
+++
@@ -1,7 +0,0 @@
-class TechnologyConstructionAssessmentMethodService
-
-    def self.get_all
-        return TechnologyConstructionAssessmentMethod.select('id, name')
-    end
-
-end
