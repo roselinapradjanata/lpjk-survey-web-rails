43cb5cb66e6a0b2e3136ef671bca6b6c01f71eaa
Roselina Pradjanata <13515034@std.stei.itb.ac.id>
2018-09-13 16:05:21 +0700

    Create one to one association to user and company

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_one :company
+
   validates_presence_of :email, :password_digest
   validates :email, uniqueness: true
-
-  has_one :company
 
   has_secure_password
 end
